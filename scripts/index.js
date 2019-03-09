d6ebf0081b7e1997f0de85ca8df50d0f83161ffa
Rodrigo <rcoto@protonmail.com>
2019-03-08 21:56:06 -0800

    feat: MomentJs has been implemented

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+var moment = require("moment");
+moment().format();
+
 const workIncrease = document.querySelector("#increase-work");
 const workDecrease = document.querySelector("#decrease-work");
 const workDisplay = document.querySelector("#work-display");
@@ -5,8 +8,16 @@ const breakIncrease = document.querySelector("#increase-break");
 const breakDecrease = document.querySelector("#decrease-break");
 const breakDisplay = document.querySelector("#break-display");
 
+const setTime = minutes =>
+  moment()
+    .second(0)
+    .add(minutes, "minutes");
+
 let workAdjuster = 25;
 let breakAdjuster = 5;
+
+let workTimer = setTime(workAdjuster);
+let breakTimer = setTime(breakAdjuster);
 
 const render = (elementDisplay, itemRender) => {
   elementDisplay.innerHTML = itemRender;
@@ -26,25 +37,30 @@ const changeAdjuster = (adjuster, action) => {
   return result;
 };
 
+// Initial Render of minutes
 render(workDisplay, workAdjuster);
 render(breakDisplay, breakAdjuster);
 
 workIncrease.addEventListener("click", function() {
   workAdjuster = changeAdjuster(workAdjuster, "add");
-  render(workDisplay, workAdjuster);
+  workTimer = setTime(workAdjuster);
+  workTimer = render(workDisplay, workAdjuster);
 });
 
 workDecrease.addEventListener("click", function() {
   workAdjuster = changeAdjuster(workAdjuster, "subtract");
+  workTimer = setTime(workAdjuster);
   render(workDisplay, workAdjuster);
 });
 
 breakIncrease.addEventListener("click", function() {
   breakAdjuster = changeAdjuster(breakAdjuster, "add");
+  breakTimer = setTime(breakAdjuster);
   render(breakDisplay, breakAdjuster);
 });
 
 breakDecrease.addEventListener("click", function() {
   breakAdjuster = changeAdjuster(breakAdjuster, "subtract");
+  breakTimer = setTime(breakAdjuster);
   render(breakDisplay, breakAdjuster);
 });
